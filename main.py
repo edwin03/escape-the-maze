ab6cdc88877ab28da3dac243a1888d81d3799bf7
Edwin Rubio <edwin.rubio@ymail.com>
2026-05-05 23:07:19 -0500

    Finished the project.

---
+++ main.py
@@ -0,0 +1,16 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    elif wall_on_right():
+        if front_is_clear():
+            move()
+        else:
+            turn_left()
